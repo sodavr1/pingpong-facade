91cfcfbbd794b852c22aef8f3379b3f454f9f692
sodavr1 <152621685+sodavr1@users.noreply.github.com>
2023-12-08 11:15:05 +0000

    Update main.js
    socket version

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-import { io } from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js";
+import { io } from "https://cdn.socket.io/4.7.2/socket.io.esm.min.js";
 import Ball from "./ball.js";
 import Player from "./player.js";
 
