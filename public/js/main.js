3a0dd01bfc7ec50ee9dc632b341f5270032589a7
sodavr1 <sodavronestation@gmail.com>
2023-12-11 09:24:20 +0000

    # added aaron's code for pong - seperated out css and js - removed ball and player - all contained in main now

--- public/js/main.js
+++ public/js/main.js
@@ -1,122 +1,158 @@
-import { io } from "https://cdn.socket.io/4.7.2/socket.io.esm.min.js";
-import Ball from "./ball.js";
-import Player from "./player.js";
+const canvas = document.getElementById('game');
+const context = canvas.getContext('2d');
+const grid = 15;
+const paddleHeight = grid * 5; // 80
+const maxPaddleY = canvas.height - grid - paddleHeight;
 
-let startBtn = document.getElementById('startBtn');
-startBtn.addEventListener('click', startGame);
+var paddleSpeed = 9;
+var ballSpeed = 5;
+var leftPlayerScore = 0;
+var rightPlayerScore = 0;
+var gameStarted = false;
 
-let message = document.getElementById('message');
-let canvas = document.getElementById('canvas');
-let ctx = canvas.getContext('2d');
+const leftPaddle = {
+    x: grid * 2,
+    y: canvas.height / 2 - paddleHeight / 2,
+    width: grid,
+    height: paddleHeight,
+    dy: 0
+};
+const rightPaddle = {
+    x: canvas.width - grid * 3,
+    y: canvas.height / 2 - paddleHeight / 2,
+    width: grid,
+    height: paddleHeight,
+    dy: 0
+};
+const ball = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    width: grid,
+    height: grid,
+    backgroundColor: 'red',
+    resetting: false,
+    dx: ballSpeed,
+    dy: -ballSpeed
+};
 
-let player1;
-let player2;
-let ball;
-
-let isGameStarted = false;
-let playerNo = 0;
-let roomID;
-
-const socket = io("http://localhost:10000", {
-    transports: ['websocket']
-});
-
-function startGame() {
-    startBtn.style.display = 'none';
-
-    if (socket.connected) {
-        socket.emit('join');
-        message.innerText = "Waiting for other player..."
-    }
-    else {
-        message.innerText = "Refresh the page and try again..."
-    }
+function collides(obj1, obj2) {
+    return obj1.x < obj2.x + obj2.width &&
+        obj1.x + obj1.width > obj2.x &&
+        obj1.y < obj2.y + obj2.height &&
+        obj1.y + obj1.height > obj2.y;
 }
 
-socket.on("playerNo", (newPlayerNo) => {
-    console.log(newPlayerNo);
-    playerNo = newPlayerNo;
-});
+function drawScores() {
+    context.fillStyle = 'white';
+    context.font = '24px Arial';
+    context.fillText('Player 1: ' + leftPlayerScore, 20, 30);
+    context.fillText('Player 2: ' + rightPlayerScore, canvas.width - 150, 30);
+}
 
-socket.on("startingGame", () => {
-    isGameStarted = true;
-    message.innerText = "We are going to start the game...";
-});
+function loop() {
+    requestAnimationFrame(loop);
+    context.clearRect(0, 0, canvas.width, canvas.height);
 
-socket.on("startedGame", (room) => {
-    console.log(room);
+    if (gameStarted) {
+        leftPaddle.y += leftPaddle.dy;
+        rightPaddle.y += rightPaddle.dy;
 
-    roomID = room.id;
-    message.innerText = "";
-
-    player1 = new Player(room.players[0].x, room.players[0].y, 20, 60, 'red');
-    player2 = new Player(room.players[1].x, room.players[1].y, 20, 60, 'blue');
-
-    player1.score = room.players[0].score;
-    player2.score = room.players[1].score;
-
-
-    ball = new Ball(room.ball.x, room.ball.y, 10, 'white');
-
-    window.addEventListener('keydown', (e) => {
-        if (isGameStarted) {
-            if (e.keyCode === 38) {
-                console.log("player move 1 up")
-                socket.emit("move", {
-                    roomID: roomID,
-                    playerNo: playerNo,
-                    direction: 'up'
-                })
-            } else if (e.keyCode === 40) {
-                console.log("player move 1 down")
-                socket.emit("move", {
-                    roomID: roomID,
-                    playerNo: playerNo,
-                    direction: 'down'
-                })
-            }
+        if (leftPaddle.y < grid) {
+            leftPaddle.y = grid;
+        } else if (leftPaddle.y > maxPaddleY) {
+            leftPaddle.y = maxPaddleY;
         }
-    });
 
-    draw();
-});
+        if (rightPaddle.y < grid) {
+            rightPaddle.y = grid;
+        } else if (rightPaddle.y > maxPaddleY) {
+            rightPaddle.y = maxPaddleY;
+        }
 
-socket.on("updateGame", (room) => {
-    player1.y = room.players[0].y;
-    player2.y = room.players[1].y;
+        context.fillStyle = 'white';
+        context.fillRect(leftPaddle.x, leftPaddle.y, leftPaddle.width, leftPaddle.height);
+        context.fillRect(rightPaddle.x, rightPaddle.y, rightPaddle.width, rightPaddle.height);
 
-    player1.score = room.players[0].score;
-    player2.score = room.players[1].score;
+        ball.x += ball.dx;
+        ball.y += ball.dy;
 
-    ball.x = room.ball.x;
-    ball.y = room.ball.y;
+        if (ball.y < grid) {
+            ball.y = grid;
+            ball.dy *= -1;
+        } else if (ball.y + grid > canvas.height - grid) {
+            ball.y = canvas.height - grid * 2;
+            ball.dy *= -1;
+        }
 
-    draw();
-});
+        if ((ball.x < 0 || ball.x > canvas.width) && !ball.resetting) {
+            if (ball.x < 0) {
+                rightPlayerScore++;
+            } else {
+                leftPlayerScore++;
+            }
+            ball.resetting = true;
 
-socket.on("endGame", (room) => {
-    isGameStarted = false;
-    message.innerText = `${room.winner === playerNo ? playerNo+'wins' : "You Lost!"}`;
+            setTimeout(() => {
+                ball.resetting = false;
+                ball.x = canvas.width / 2;
+                ball.y = canvas.height / 2;
+            }, 400);
+        }
 
-    socket.emit("leave", roomID);
+        drawScores(); // Update scores on canvas
 
-    setTimeout(() => {
-        ctx.clearRect(0, 0, 800, 500);
-        startBtn.style.display = 'block';
-    }, 2000);
-});
+        if (collides(ball, leftPaddle)) {
+            ball.dx *= -1;
+            ball.x = leftPaddle.x + leftPaddle.width;
+        } else if (collides(ball, rightPaddle)) {
+            ball.dx *= -1;
+            ball.x = rightPaddle.x - ball.width;
+        }
 
-function draw() {
-    ctx.clearRect(0, 0, 800, 500);
-    player1.draw(ctx);
-    player2.draw(ctx);
-    ball.draw(ctx);
+        context.fillRect(ball.x, ball.y, ball.width, ball.height);
+        context.fillStyle = 'lightgrey';
+        context.fillRect(0, 0, canvas.width, grid);
+        context.fillRect(0, canvas.height - grid, canvas.width, canvas.height);
 
-    // center line
-    ctx.strokeStyle = 'white';
-    ctx.beginPath();
-    ctx.setLineDash([10, 10])
-    ctx.moveTo(400, 5);
-    ctx.lineTo(400, 495);
-    ctx.stroke();
+        for (let i = grid; i < canvas.height - grid; i += grid * 2) {
+            context.fillRect(canvas.width / 2 - grid / 2, i, grid, grid);
+        }
+    } else {
+        context.fillStyle = 'white';
+        context.font = '30px Arial';
+        context.fillText('Click Start Game to Play', 200, canvas.height / 2);
+    }
 }
+
+document.getElementById('startButton').addEventListener('click', function() {
+    gameStarted = true;
+    canvas.style.display = 'block';
+    this.style.display = 'none';
+    requestAnimationFrame(loop);
+});
+
+document.addEventListener('keydown', function(e) {
+    if (e.which === 38) {
+        rightPaddle.dy = -paddleSpeed;
+    } else if (e.which === 40) {
+        rightPaddle.dy = paddleSpeed;
+    }
+
+    if (e.which === 87) {
+        leftPaddle.dy = -paddleSpeed;
+    } else if (e.which === 83) {
+        leftPaddle.dy = paddleSpeed;
+    }
+});
+
+document.addEventListener('keyup', function(e) {
+    if (e.which === 38 || e.which === 40) {
+        rightPaddle.dy = 0;
+    }
+
+    if (e.which === 83 || e.which === 87) {
+        leftPaddle.dy = 0;
+    }
+});
+
+requestAnimationFrame(loop);
