0bce5617b590c3c3913944d6472eafa48c357fdb
sodavr1 <chrispgodber@gmail.com>
2023-12-28 06:42:12 +0000

    - implemented user num list

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ import { Server } from 'socket.io';
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+    allowedHeaders: ["game-header"],
+    credentials: true
+  }
+});
 
 // Serve static files from the 'public' folder
 app.use(express.static("public"));
@@ -26,10 +32,7 @@ io.on('connection', (socket) => {
             // Join the specified room
             socket.join(roomName);
             console.log('roomename'+roomName)
-            // console.log(io.sockets.adapter.rooms);
-
-            // Emit the 'playerJoined' event to all clients in the room
-            // io.to(roomName).emit('playerJoined', io.sockets.adapter.rooms[roomName].length);
+            io.to(roomName).emit('userList', getUsersInRoom(roomName));
         });
 
         // Event listener for the 'startGame' event
@@ -43,8 +46,20 @@ io.on('connection', (socket) => {
         socket.on('disconnect', () => {
             console.log('A user disconnected');
         });
+
+        
     });
 });
+
+function getUsersInRoom(room) {
+  const clients = io.sockets.adapter.rooms.get(room);
+  if (clients) {
+    return Array.from(clients).map((clientId) => io.sockets.sockets.get(clientId).id);
+  } else {
+    return [];
+  }
+}
+
 
 
 // Start the server on port 3000
