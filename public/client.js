f3e8d7514a2e7fd1363fddf275e1c1af952ff2ac
sodavr1 <sodavronestation@gmail.com>
2024-01-09 18:01:25 +0000

    - game loop running again - changed db port to 3000 - readdded http to requests - test on deploy

--- public/client.js
+++ public/client.js
@@ -1,15 +1,18 @@
 import { io } from "socket.io-client";
 
-const port = 3000;
-const socket = io(`ws://localhost:${port}`);
+const port = process.env.PORT || 3000;
+const socket = io(`http://localhost:${port}`);
 
 socket.emit("chat message", "Hello, server!");
+
 socket.on("connect", () => {
   console.log(`connect ${socket.id}`);
 });
+
 socket.on("connect_error", (err) => {
   console.log(`connect_error due to ${err.message}`);
 });
+
 socket.on("disconnect", (reason) => {
   console.log(`disconnect due to ${reason}`);
 });
--- server.js
+++ server.js
@@ -1,38 +1,54 @@
 // Import required modules using ES6 import syntax
 import express from 'express';
-import { createServer } from 'http';
+import {createServer} from 'http';
 import { Server } from 'socket.io';
+
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "localhost:3000",
+    origin: "http://localhost:3000",
     allowedHeaders: ["game-header"],
     credentials: true
   }
 });
+
 // Serve static files from the 'public' folder
 app.use(express.static("public"));
+// Socket.IO connection event
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  // Example: Handling disconnection
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
   // Handle socket connections
   io.on('connection', (socket) => {
     console.log('A user connected');
+
     // Event listener for the 'join' event
     socket.on('joinRoom', (roomName) => {
       // Join the specified room
       socket.join(roomName);
+      console.log('roomename'+roomName)
       io.to(roomName).emit('userList', getUsersInRoom(roomName));
     });
+
     // Event listener for the 'startGame' event
     socket.on('startGame', () => {
       // Emit the 'startGame' event to all clients in the room
       const roomName = Object.keys(socket.rooms)[1]; // Get the room name
       io.to(roomName).emit('startGame');
     });
+
     // Handle disconnections
     socket.on('disconnect', () => {
       console.log('A user disconnected');
     });
+
+
   });
+});
 
 function getUsersInRoom(room) {
   const clients = io.sockets.adapter.rooms.get(room);
@@ -43,8 +59,8 @@ function getUsersInRoom(room) {
   }
 }
 
-// Start the server on port 3001
-const PORT_SERVER = process.env.PORT_SERVER || 3001;
-httpServer.listen(PORT_SERVER, () => {
-  console.log(`Server is running on port ${PORT_SERVER}`);
+// Start the server on port 3000
+const PORT = process.env.PORT || 3000;
+httpServer.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
